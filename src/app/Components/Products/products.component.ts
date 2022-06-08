d73556839ca6c116c3ba2442b072ed7196aa7cec
josephmonreal00 <joemonreal23@icloud.com>
2022-06-08 08:41:18 -0700

    3 files modified

--- src/app/Components/Products/products.component.ts
+++ src/app/Components/Products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'Products',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['products.component.css'],
 })
 export class Products {
+  readonly ROOT_URL = 'https://jsonplaceholder.typicode.com';
+  posts: any;
   products: string = 'Products';
+  constructor(private http: HttpClient) {}
+
+  getPosts() {
+    this.posts = this.http.get(this.ROOT_URL + '/posts');
+  }
 }
