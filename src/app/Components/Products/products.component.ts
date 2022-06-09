427023e31de65725374b0de1966c05fb17038b0e
josephmonreal00 <joemonreal23@icloud.com>
2022-06-08 18:36:31 -0700

    3 files modified, 1 added

--- src/app/Components/Products/products.component.ts
+++ src/app/Components/Products/products.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { Post } from './post';
+import { Observable, pipe, map, tap, take } from 'rxjs';
 
 @Component({
   selector: 'Products',
@@ -8,11 +10,39 @@ import { HttpClient } from '@angular/common/http';
 })
 export class Products {
   readonly ROOT_URL = 'https://jsonplaceholder.typicode.com';
-  posts: any;
+  readonly MEMEURL = 'https://api.imgflip.com/get_memes';
+  memes: any;
+  posts: Observable<Post[]>;
   products: string = 'Products';
+  newPost: Observable<any>;
+  theMemes: any;
+
   constructor(private http: HttpClient) {}
 
   getPosts() {
-    this.posts = this.http.get(this.ROOT_URL + '/posts');
+    let params = new HttpParams().set('userId', '1');
+    let headers = new HttpHeaders().set('Authorization', 'auth-token');
+
+    this.posts = this.http.get<Post[]>(this.ROOT_URL + '/posts', { params });
+
+    this.memes = this.http.get(this.MEMEURL).subscribe({
+      next: (meme) => console.log(meme),
+      error: (err) => console.log(err),
+      complete: () => console.log('Complete'),
+    });
+  }
+
+  createPost() {
+    const data: Post = {
+      id: null,
+      userId: 23,
+      title: 'Joes new post',
+      body: 'Recently got injured',
+    };
+
+    this.newPost = this.http.post(this.ROOT_URL + '/posts', data).pipe(
+      map((item) => console.log(item)),
+      tap((item) => console.log(item))
+    );
   }
 }
